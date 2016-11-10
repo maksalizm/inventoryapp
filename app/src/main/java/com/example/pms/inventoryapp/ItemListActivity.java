a3e0d3cc0fbb298afff278572ca282ac994e9f84
pms <pms@wishket.com>
2016-11-10 17:00:30 +0900

    first commit

---
+++ app/src/main/java/com/example/pms/inventoryapp/ItemListActivity.java
@@ -0,0 +1,13 @@
+package com.example.pms.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+    }
+}
